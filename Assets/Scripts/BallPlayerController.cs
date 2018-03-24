041422e6cf1b72663a2317349c7e739ca36b0339
Ravi Le <deadlove011011@gmail.com>
2018-03-25 04:28:21 +0700

    test

--- Assets/Scripts/BallPlayerController.cs
+++ Assets/Scripts/BallPlayerController.cs
@@ -2,15 +2,45 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BallPlayerController : MonoBehaviour {
+public class BallPlayerController : MonoBehaviour
+{
+
+    [SerializeField] private float moveSpeed = 1;
+
+
+    private Rigidbody rigBody;
+    void Awake()
+    {
+        rigBody = GetComponent<Rigidbody>();
+    }
 
 	// Use this for initialization
 	void Start () {
-		
+		rigBody.velocity = new Vector3(moveSpeed, 0 ,0);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+	    if (Input.GetMouseButtonDown(0))
+	    {
+	        print("change direction");
+	        SwitchDirection();
+	    }
+    }
+
+    void FixedUpdate()
+    {
+      
+    }
+
+    void SwitchDirection()
+    {
+        if (rigBody.velocity.z > 0)
+        {
+            rigBody.velocity = new Vector3(moveSpeed, 0, 0);
+        }else if (rigBody.velocity.x > 0)
+        {
+            rigBody.velocity = new Vector3(0, 0, moveSpeed);
+        }
+    }
 }
